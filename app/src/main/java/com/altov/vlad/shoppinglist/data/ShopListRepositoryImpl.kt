6bb882de5cb3930e9708ddbf506cb21c77f68af2
Vladimir Aushov <126251385+SamuraAlt@users.noreply.github.com>
2023-08-03 18:27:37 +0700

    presentation layer. List auto update

--- app/src/main/java/com/altov/vlad/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/altov/vlad/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,22 +1,35 @@
 package com.altov.vlad.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.altov.vlad.shoppinglist.domain.ShopItem
 import com.altov.vlad.shoppinglist.domain.ShopListRepository
 
 object ShopListRepositoryImpl:ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10){
+            val item = ShopItem("Name $i",i.toDouble(),true)
+            addShopItem(item)
+        }
+    }
     override fun addShopItem(shopItem: ShopItem) {
         if(shopItem.id == ShopItem.UNDEFINED_ID){
         shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -31,7 +44,10 @@ object ShopListRepositoryImpl:ShopListRepository {
         } ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/altov/vlad/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/altov/vlad/shoppinglist/presentation/MainActivity.kt
@@ -2,13 +2,22 @@ package com.altov.vlad.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.altov.vlad.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this){
+            Log.d("MainActivityTest",it.toString())
+
+        }
     }
 }
