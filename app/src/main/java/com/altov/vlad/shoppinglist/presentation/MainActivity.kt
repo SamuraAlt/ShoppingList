d54a83cde5664be59f06d02ae91411172b3130d3
Vladimir Aushov <126251385+SamuraAlt@users.noreply.github.com>
2023-08-11 22:35:13 +0700

    Added layouts for list items. Updated list adapter

--- app/src/main/java/com/altov/vlad/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/altov/vlad/shoppinglist/presentation/MainActivity.kt
@@ -2,22 +2,39 @@ package com.altov.vlad.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.altov.vlad.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+    private lateinit var shopListAdapter: ShopListAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.shopList.observe(this){
-            Log.d("MainActivityTest",it.toString())
-
+        viewModel.shopList.observe(this) {
+            shopListAdapter.shopList = it
         }
+    }
+
+    private fun setupRecyclerView() {
+        shopListAdapter = ShopListAdapter()
+        with(binding.rvShopList) {
+            adapter = shopListAdapter
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
+
     }
 }
