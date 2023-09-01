26d9057de616a8fd73c30f5e7cc684beb9c002d4
Vladimir Aushov <126251385+SamuraAlt@users.noreply.github.com>
2023-09-02 06:50:18 +0700

    finish second activity

--- app/src/main/java/com/altov/vlad/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/altov/vlad/shoppinglist/presentation/ShopItemActivity.kt
@@ -1,14 +1,163 @@
 package com.altov.vlad.shoppinglist.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.altov.vlad.shoppinglist.R
 import com.altov.vlad.shoppinglist.databinding.ActivityShopItemBinding
+import com.altov.vlad.shoppinglist.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityShopItemBinding
+
+    private lateinit var binding: ActivityShopItemBinding
+    private lateinit var viewModel: ShopItemViewModel
+    private var screenMode = MODE_UNKNOWN
+    private var shopItemId = ShopItem.UNDEFINED_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShopItemBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+        parsIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        addTextChangeListeners()
+        launchRightMode()
+        observeViewModel()
+
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputName.observe(this) {
+            val message = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            binding.etName.error = message
+
+        }
+        viewModel.errorInputCount.observe(this) {
+            val message = if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            binding.etCount.error = message
+        }
+
+
+        viewModel.activityReadyToClose.observe(this) {
+            finish()
+
+        }
+    }
+
+    private fun launchRightMode() {
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    private fun addTextChangeListeners() {
+        binding.etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputName()
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+        binding.etCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputCount()
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+
+    private fun launchEditMode() {
+        viewModel.getShopItem(shopItemId)
+        viewModel.shopItem.observe(this) {
+            binding.etName.setText(it.name)
+            binding.etCount.setText(it.count.toString())
+        }
+        binding.saveButton.setOnClickListener {
+            viewModel.editShopItem(
+                binding.etName.text?.toString(),
+                binding.etCount.text?.toString()
+            )
+
+
+        }
+
+    }
+
+    private fun launchAddMode() {
+        binding.saveButton.setOnClickListener {
+            viewModel.addShopItem(binding.etName.text?.toString(), binding.etCount.text?.toString())
+
+
+        }
+    }
+
+
+    private fun parsIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is absent")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_ADD && mode != MODE_EDIT) {
+            throw RuntimeException("Unknown screen mode $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Param shop item id is absent")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_UNKNOWN = ""
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, itemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, itemId)
+            return intent
+        }
     }
 }
--- app/src/main/java/com/altov/vlad/shoppinglist/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/altov/vlad/shoppinglist/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.altov.vlad.shoppinglist.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.altov.vlad.shoppinglist.data.ShopListRepositoryImpl
 import com.altov.vlad.shoppinglist.domain.AddShopItemUseCase
@@ -7,57 +9,99 @@ import com.altov.vlad.shoppinglist.domain.EditShopItemUseCase
 import com.altov.vlad.shoppinglist.domain.GetShopItemUseCase
 import com.altov.vlad.shoppinglist.domain.ShopItem
 
-class ShopItemViewModel:ViewModel() {
+class ShopItemViewModel : ViewModel() {
 
-    private  val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl
 
-   private val getShopItemUseCase = GetShopItemUseCase(repository)
-   private val addShopItemUseCase = AddShopItemUseCase(repository)
-   private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    fun getShopItem(shopItemId:Int){
+    private val _errorInputName = MutableLiveData<Boolean>()
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    private val _shopItem = MutableLiveData<ShopItem>()
+    private val _activityReadyToClose = MutableLiveData<Unit>()
+    val errorInputName:LiveData<Boolean>
+        get() {
+            return _errorInputName
+        }
+    val errorInputCount:LiveData<Boolean>
+        get() {
+            return _errorInputCount
+        }
+    val shopItem:LiveData<ShopItem>
+        get(){
+            return _shopItem
+        }
+    val activityReadyToClose:LiveData<Unit>
+        get(){
+            return _activityReadyToClose
+        }
+
+    fun getShopItem(shopItemId: Int) {
         val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
+
     }
-    fun addShopItem(inputName:String?,inputCount:String?){
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
         val name = parsName(inputName)
         val count = parsCount(inputCount)
-        val fieldsValid = validateInput(name,count)
-        if(fieldsValid){
-            val shopItem = ShopItem(name,count,true)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
 
     }
-    fun editShopItem(inputName:String?,inputCount:String?){
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
         val name = parsName(inputName)
         val count = parsCount(inputCount)
-        val fieldsValid = validateInput(name,count)
-        if(fieldsValid){
-            val shopItem = ShopItem(name,count,true)
-            editShopItemUseCase.editShopItem(shopItem)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            _shopItem.value?.let {
+                val item = it.copy(name = name,count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork() }
+
         }
     }
-    private fun parsName(inputName:String?):String{
+
+
+    private fun parsName(inputName: String?): String {
         return inputName?.trim() ?: ""
     }
-    private fun parsCount(inputCount: String?):Double{
+
+    private fun parsCount(inputCount: String?): Int {
         return try {
-            val count = inputCount?.trim()?.toDouble() ?: 0.0
+            val count = inputCount?.trim()?.toInt() ?: 0
             count
-        } catch (e:Exception){
-            0.0
+        } catch (e: Exception) {
+            0
         }
     }
-    private fun validateInput(name:String,count:Double):Boolean {
+
+    private fun validateInput(name: String, count: Int): Boolean {
         var result = true
-        if(name.isBlank()){
-            //TODO:Show error input name
+        if (name.isBlank()) {
+            _errorInputName.value = true
             result = false
         }
-        if(count<=0.0){
-            //TODO:Show error input count
+        if (count <= 0.0) {
+            _errorInputCount.value = true
             result = false
         }
         return result
+    }
+    fun resetErrorInputName(){
+        _errorInputName.value = false
+    }
+    fun resetErrorInputCount(){
+        _errorInputCount.value = false
+    }
+    private fun finishWork(){
+        _activityReadyToClose.value = Unit
     }
 }
